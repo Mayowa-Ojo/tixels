4f14e5c8ce0ea7706ae221cbe14b06c5e741f69a
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-04-26 11:19:28 -0600

    feat: pass message to content script

--- src/background/index.js
+++ src/background/index.js
@@ -1,1 +1,13 @@
+chrome.runtime.onInstalled.addListener(function() {
+   console.log("loading...")
 
+   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+         conditions: [new chrome.declarativeContent.PageStateMatcher({
+               pageUrl: {hostEquals: 'github.com'},
+            })
+         ],
+         actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+   });
+})
