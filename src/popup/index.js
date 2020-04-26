d9294978955be8a0b56f05335abae52c8088b8d5
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-04-26 11:21:02 -0600

    bug: page action fails

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,1 +1,12 @@
 import 'alpinejs'
+
+const button = document.querySelector("button")
+
+button.onclick = function(element) {
+   chrome.tabs.query({"active": true, "currentWindow": true}, function(tabs) {
+      chrome.tabs.executeScript(
+         tabs[0].id,
+         {code: 'document.body.style.backgroundColor = "#C1C1C1"'}
+      )
+   })
+}
