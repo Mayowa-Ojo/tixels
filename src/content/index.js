e4926df3c7aaafccfc16ad389e5026fb628a20ab
Mayowa Ojo <mayowaojo.e@gmail.com>
2020-04-26 11:20:05 -0600

    feat: send message to popup.js

--- src/content/index.js
+++ src/content/index.js
@@ -1,2 +1,18 @@
-import 'alpinejs'
+import 'alpinejs';
 
+console.log("working...")
+(function() {
+   function script() {
+      console.log(window.location);
+   }
+
+   function inject(fn) {
+      const script = document.createElement("script");
+
+      script.textContent = `(${fn.toString()})();`
+
+      (document.head || document.documentElement).appendChild(script);
+   }
+
+   inject(script);
+});
